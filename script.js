33b318a2733b2a966759845c2a9161501bf3b7f8
mkayf <muhammadkaif150@gmail.com>
2024-04-29 00:48:03 +0500

    Javascript task 1 assigned

---
+++ script.js
@@ -0,0 +1,7 @@
+// Task-1 - Celcius to Fahrenheit and vice versa
+// 1.(A) Ek function banao jo celcius value lega argument mein, us value ko fahrenheit mein convert karke console mein print karwao.
+
+// 1.(B) Ek function banao jo fahrenheit value lega argument mein, us value ko celcius value mein convert karke console mein print karwao.
+
+// FORMULA FOR CELCIUS TO FAHRENHEIT: (Celcius value * 9/5) + 32
+// FORMULA FOR FAHRENHEIT TO CELCIUS: (Fahrenheit value - 32) * 5/9
