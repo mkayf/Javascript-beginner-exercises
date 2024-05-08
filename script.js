83c2720d888f4c4126e2553612303a2218f20daf
mkayf <muhammadkaif150@gmail.com>
2024-05-09 00:14:18 +0500

    Abdul Samad completed first task

--- script.js
+++ script.js
@@ -5,3 +5,26 @@
 
 // FORMULA FOR CELCIUS TO FAHRENHEIT: (Celcius value * 9/5) + 32
 // FORMULA FOR FAHRENHEIT TO CELCIUS: (Fahrenheit value - 32) * 5/9
+
+// Please submit tasks in script.js file not in index.html file;
+
+// Abdul Samad:
+
+    // first Question solved 
+
+    function CelciusToFerhenheit(m){
+        return (m * 9/5) + 32;
+    }        
+    let rezult = CelciusToFerhenheit(89.6)   
+        console.log(rezult);
+
+    // Second Question solved 
+
+    function ferhenheitToCelcius(n){
+        return (n-32) * 5/9;
+    }
+
+    let  rezult2 = ferhenheitToCelcius(89.6);
+    console.log(rezult2);
+
+    // compelte 
